d6ddd2be2978f47de2465471e1c688c688d4a874
makenaJun <makena.jun@gmail.com>
2022-08-01 18:43:09 +0300

    add start script and get port in env

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!!sdsds!')
+    res.send('Hello World!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
 })
